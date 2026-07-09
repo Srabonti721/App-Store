8c759084c0142202d7498d774fb051db6241dd04
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-07-09 11:10:13 +0600

    added hero section and json data

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,39 +3,40 @@ import { FaFacebook, FaGithub } from 'react-icons/fa';
 import { TbBrandYoutubeFilled } from 'react-icons/tb';
 
 const Footer = () => {
-    return (
-<footer className="footer sm:footer-horizontal bg-linear-to-r from-indigo-200 from-10% via-sky-200 via-30% to-emerald-400 to-90%  p-10">
-  <nav>
-    <h6 className="footer-title font-bold text-xl">Terms of Service</h6>
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover ">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
-  </nav>
-  <nav>
-    <h6 className="footer-title font-bold text-xl">Privacy Policy</h6>
-    <a className="link link-hover">Email</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Users</a>
-  </nav>
-  <nav>
-    <h6 className="footer-title font-bold text-xl">Developer Resources</h6>
-    <a className="link link-hover">Documentation</a>
-    <a className="link link-hover">Support</a>
-    <a className="link link-hover">GitHub Repository</a>
-  </nav>
-  <nav>
-    <h6 className="footer-title font-bold text-xl  ">Social Media</h6>
-    <div className='flex gap-4' >
-    <a className="link link-hover"><FaGithub size={28}/></a>
-    <a className="link link-hover text-blue-700"><FaFacebook size={28} /></a>
-    <a className="link link-hover text-red-700"><TbBrandYoutubeFilled size={28}/></a>
-    </div>
-
-  </nav>
-</footer>
-    );
+  return (
+    <footer className="footer sm:footer-horizontal bg-linear-to-r from-indigo-200 from-10% via-sky-200 via-30% to-emerald-400 to-90%  p-10">
+      <nav>
+        <h6 className="footer-title font-bold text-xl">Terms of Service</h6>
+        <a className="link link-hover">Terms & Conditions</a>
+        <a className="link link-hover">Acceptable Use</a>
+        <a className="link link-hover ">License</a>
+        <a className="link link-hover">Refund Policy</a>
+        <a className="link link-hover">User Agreement</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title font-bold text-xl">Privacy Policy</h6>
+        <a className="link link-hover">Privacy Policy</a>
+        <a className="link link-hover">Cookie Policy</a>
+        <a className="link link-hover">Data Collection</a>
+        <a className="link link-hover">User Privacy</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title font-bold text-xl">Developer Resources</h6>
+        <a className="link link-hover">Documentation</a>
+        <a className="link link-hover">Support</a>
+        <a className="link link-hover">GitHub Repository</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title font-bold text-xl  ">Social Media</h6>
+        <div className='flex gap-4' >
+          <a className="link link-hover"><FaGithub size={28} /></a>
+          <a className="link link-hover text-blue-700"><FaFacebook size={28} /></a>
+          <a className="link link-hover text-red-700"><TbBrandYoutubeFilled size={28} /></a>
+        </div>
+        <p> © 2026 AppStore. All Rights Reserved.</p>
+      </nav>
+    </footer>
+  );
 };
 
 export default Footer;
---
+++ src/Components/HomePageComponents/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import heroAps from '../../assets/more apps.png'
+const Hero = () => {
+    return (
+        <div className="hero my-8">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img
+                    src={heroAps}
+                    className="max-w-lg rounded-lg"
+                />
+                <div>
+                    <h1 className="text-3xl font-bold"> <span className='text-indigo-500 text-4xl'>Find</span>, <span className='text-emerald-400'>Explore</span> & Download Your Favorite Apps </h1>
+                    <p className="py-6 text-gray-400">
+                        Explore top-rated apps across multiple categories. Find the perfect app for work, learning, health, and entertainment—all in one place.
+                    </p>
+                    <button className="btn btn-primary text-black">Explore Apps</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
