5bee39c33c2f8635968847a520fd090e624d7668
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-07-12 01:26:31 +0600

    sign up with google popup

--- src/Authentication/forms/Register.jsx
+++ src/Authentication/forms/Register.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../../Context/AuthProvider';
 
 const Register = () => {
+    const {googleSignIn} = use(AuthContext);
     const handleRegister = e => {
         e.preventDefault();
         const name = e.target.name.value;
@@ -10,6 +12,18 @@ const Register = () => {
         const password = e.target.password.value;
         console.log(name, photo, email, password);
 
+    }
+
+    // google Sign in
+    const handleGoogleSign = () =>{
+        googleSignIn()
+        .then(result=>{
+            console.log(result); 
+        })
+        .error(error=>{
+            console.log(error);
+            
+        })
     }
     return (
         <div className="my-10 p-4 card bg-base-100 w-full max-w-lg mx-auto shrink-0 shadow-2xl x">
@@ -57,10 +71,10 @@ const Register = () => {
                     <button className="btn btn-neutral mt-4">Register</button>
                 </form>
                 <p>have already an account?
-                    <Link className='text-blue-800 underline text-sm' to='/authLayout/login'> Register</Link></p>
+                    <Link className='text-blue-800 underline text-sm' to='/authLayout/login'> Login</Link></p>
                 ------------------------ or -----------------------
                 {/* Google */}
-                <button className="btn hover:bg-blue-400 hover:text-white bg-white text-black border-[#e5e5e5]">
+                <button onClick={handleGoogleSign} className="btn hover:bg-blue-400 hover:text-white bg-white text-black border-[#e5e5e5]">
                     <svg aria-label="Google logo" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                     Login with Google
                 </button>
--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,11 +1,22 @@
-import React, { createContext } from 'react';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import React, { createContext, useState } from 'react';
+import { auth } from '../firebase/firebase.init';
 
 export const AuthContext = createContext();
 
+
+const GoogleProvider = new GoogleAuthProvider();
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState('');
+    // GOOGLE SIGN IN
+    const googleSignIn = () =>{
+        return signInWithPopup(auth, GoogleProvider);
+    }
 
 const userInfo = {
-    email:"mona@tona.com"
+    user,
+     setUser,
+     googleSignIn
 }
 
     return <AuthContext value={userInfo} >{children}</AuthContext>
